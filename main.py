40945bdb76b302a938484f2b7eb3301e2bb47b17
CleKraus <kraus.clemens@gmail.com>
2021-02-12 10:46:25 +0100

    Final code

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import logging
 
 import pandas as pd
+from fastapi import FastAPI, HTTPException, Query
 
 import src.preprocessing as pp
 import src.select_restaurant as sr
 
 logging.basicConfig(level=logging.DEBUG)
 
-category = "mexican"
-lng = -400
-lat = 39
+app = FastAPI()
 
 csv_path = "data//fast_food_restaurants.csv"
 
@@ -18,7 +17,26 @@ df = pd.read_csv(csv_path)
 # run preproccessing
 df = pp.run_preprocessing(df)
 
-# find the closest restaurant of the searched category
-df_closest = sr.search_restaurant(df, category, lng, lat)
 
-print(df_closest.head())
+@app.get("/")
+def get_recommendation(
+    category: str,
+    longitude: float = Query(..., gt=-180, lt=180),
+    latitude: float = Query(..., gt=-90, lt=90),
+):
+
+    # find the closest restaurant of the searched category
+    df_closest = sr.search_restaurant(df, category, longitude, latitude)
+
+    # raise an error if no restaurant were found
+    if len(df_closest) == 0:
+        raise HTTPException(status_code=404, detail="No restaurants found")
+
+    dict_out = {
+        "restaurant_name": df_closest.iloc[0]["name"],
+        "address": df_closest.iloc[0]["address"],
+        "city": df_closest.iloc[0]["city"],
+        "distance": df_closest.iloc[0]["distance"],
+        "categories": df_closest.iloc[0]["categories"],
+    }
+    return dict_out
